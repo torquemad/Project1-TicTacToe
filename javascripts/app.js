5809069ac3718211336791f0e8ef754e183a35af
Louis Moore <burntliketoast@gmail.com>
2015-03-26 09:42:34 +1100

    added player score

--- javascripts/app.js
+++ javascripts/app.js
@@ -2,8 +2,8 @@ var marker = 'x';
 var counter = 0;
 var max_turns = 9;
 
-var player1_win = 0;
-var player2_win = 0;
+var player1Score = 0;
+var player2Score = 0;
 
 
 var board = [
@@ -28,6 +28,8 @@ function reset() {
 };
 
 $('li').on('click', function() {
+
+
     var clickedSquare = this.id.split('-')
 
     xCoOrd = parseInt(clickedSquare[0])
@@ -102,16 +104,26 @@ $('li').on('click', function() {
             $('#2-0').css("background-color", "rgb(255, 146, 37)")
             $('#2-1').css("background-color", "rgb(255, 146, 37)")
             $('#2-2').css("background-color", "rgb(255, 146, 37)")
+            player2Score++
+            $('player1Score').html(player2Score)
         } else {
             $('#0-0').css("background-color", "rgb(255, 146, 37)")
             $('#0-2').css("background-color", "rgb(255, 146, 37)")
             $('#1-1').css("background-color", "rgb(255, 146, 37)")
             $('#2-0').css("background-color", "rgb(255, 146, 37)")
             $('#2-2').css("background-color", "rgb(255, 146, 37)")
+            player1Score++
+            $('player1Score').html(player1Score)
         };
-        
+
         alert(marker + ' wins')
     };
+
+  console.log(board[0])
+  console.log(board[1])
+  console.log(board[2])
+  console.log('--------------------')
+
 });
 
 $('.btn').click(function() {
