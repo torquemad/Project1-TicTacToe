73f79566d11f5d1ee580ee3bfec23f5e062da591
Louis Moore <burntliketoast@gmail.com>
2015-03-26 09:29:56 +1100

    Updated HTML class names & added some basic CSS styling for the <li>'s

--- javascripts/app.js
+++ javascripts/app.js
@@ -7,96 +7,113 @@ var player2_win = 0;
 
 
 var board = [
-  ["+","+","+"],
-  ["+","+","+"],
-  ["+","+","+"]
-  ];
+    ["+", "+", "+"],
+    ["+", "+", "+"],
+    ["+", "+", "+"]
+];
 
-function reset(){
+function reset() {
 
-board = [
-  ["+","+","+"],
-  ["+","+","+"],
-  ["+","+","+"]
-  ];
-$('li').removeClass('selected');
+    board = [
+        ["+", "+", "+"],
+        ["+", "+", "+"],
+        ["+", "+", "+"]
+    ];
 
-$('li').html('+');
+    $('li').removeClass('selected');
+    $('li').html('+');
+    $('li').css("background-color", "")
+
 
 };
 
 $('li').on('click', function() {
-   var clickedSquare = this.id.split('-')
+    var clickedSquare = this.id.split('-')
 
     xCoOrd = parseInt(clickedSquare[0])
-    yCoOrd = parseInt(clickedSquare[1]) 
+    yCoOrd = parseInt(clickedSquare[1])
 
 
     if (counter < max_turns) {
-      counter++
+        counter++
     };
 
     if (counter % 2 === 0) {
-      marker = 'O'
-      board[xCoOrd][yCoOrd] = marker;
-      $(this).html(marker);
-      $(this).addClass('selected');
+        marker = 'O'
+        board[xCoOrd][yCoOrd] = marker;
+        $(this).html(marker);
+        $(this).addClass('selected');
     } else {
-      marker = 'X'
-      board[xCoOrd][yCoOrd] = marker;
-      $(this).html(marker)
-      $(this).addClass('selected');
+        marker = 'X'
+        board[xCoOrd][yCoOrd] = marker;
+        $(this).html(marker)
+        $(this).addClass('selected');
 
-    };  
-
-
-    if ((_.contains(board[0][0], marker) && 
-      _.contains(board[1][0], marker) && 
-      _.contains(board[2][0], marker)
-      ) ||
-
-      (_.contains(board[0][1], marker) && 
-      _.contains(board[1][1], marker) && 
-      _.contains(board[2][1], marker)
-      ) ||
-
-      (_.contains(board[0][2], marker) && 
-      _.contains(board[1][2], marker) && 
-      _.contains(board[2][2], marker)
-      ) ||
-
-      (_.contains(board[0][0], marker) && 
-      _.contains(board[0][1], marker) && 
-      _.contains(board[0][2], marker)
-      ) ||
-
-      (_.contains(board[1][0], marker) && 
-      _.contains(board[1][1], marker) && 
-      _.contains(board[1][2], marker)
-      ) ||
-
-      (_.contains(board[2][0], marker) && 
-      _.contains(board[2][1], marker) && 
-      _.contains(board[2][2], marker)
-      ) ||
-
-      (_.contains(board[2][0], marker) && 
-      _.contains(board[1][1], marker) && 
-      _.contains(board[0][2], marker)
-      ) ||
-
-      (_.contains(board[0][0], marker) && 
-      _.contains(board[1][1], marker) && 
-      _.contains(board[2][2], marker)
-      ))
-
-      {
-      alert(marker + ' wins')
-      };
+    };
 
 
+    if ((_.contains(board[0][0], marker) &&
+            _.contains(board[1][0], marker) &&
+            _.contains(board[2][0], marker)
+        ) ||
+
+        (_.contains(board[0][1], marker) &&
+            _.contains(board[1][1], marker) &&
+            _.contains(board[2][1], marker)
+        ) ||
+
+        (_.contains(board[0][2], marker) &&
+            _.contains(board[1][2], marker) &&
+            _.contains(board[2][2], marker)
+        ) ||
+
+        (_.contains(board[0][0], marker) &&
+            _.contains(board[0][1], marker) &&
+            _.contains(board[0][2], marker)
+        ) ||
+
+        (_.contains(board[1][0], marker) &&
+            _.contains(board[1][1], marker) &&
+            _.contains(board[1][2], marker)
+        ) ||
+
+        (_.contains(board[2][0], marker) &&
+            _.contains(board[2][1], marker) &&
+            _.contains(board[2][2], marker)
+        ) ||
+
+        (_.contains(board[2][0], marker) &&
+            _.contains(board[1][1], marker) &&
+            _.contains(board[0][2], marker)
+        ) ||
+
+        (_.contains(board[0][0], marker) &&
+            _.contains(board[1][1], marker) &&
+            _.contains(board[2][2], marker)
+        ))
+
+    {
+        if (marker === "O") {
+            $('#0-0').css("background-color", "rgb(255, 146, 37)")
+            $('#0-1').css("background-color", "rgb(255, 146, 37)")
+            $('#0-2').css("background-color", "rgb(255, 146, 37)")
+            $('#1-0').css("background-color", "rgb(255, 146, 37)")
+            $('#1-2').css("background-color", "rgb(255, 146, 37)")
+            $('#2-0').css("background-color", "rgb(255, 146, 37)")
+            $('#2-1').css("background-color", "rgb(255, 146, 37)")
+            $('#2-2').css("background-color", "rgb(255, 146, 37)")
+        } else {
+            $('#0-0').css("background-color", "rgb(255, 146, 37)")
+            $('#0-2').css("background-color", "rgb(255, 146, 37)")
+            $('#1-1').css("background-color", "rgb(255, 146, 37)")
+            $('#2-0').css("background-color", "rgb(255, 146, 37)")
+            $('#2-2').css("background-color", "rgb(255, 146, 37)")
+        };
+        
+        alert(marker + ' wins')
+    };
 });
 
-$('.btn').click(function(){
-  reset();
+$('.btn').click(function() {
+    reset();
 })
